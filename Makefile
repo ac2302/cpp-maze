0b83b549eaf3970b7fa7e6bb808da6dc4034a0e4
Aryan Chavan <aryan@aryansdomain.com>
2021-06-29 23:04:46 +0530

    load image

---
+++ Makefile
@@ -0,0 +1,2 @@
+build: main.cpp
+	g++ main.cpp -Wall -o solve
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+struct Node
+{
+	bool isVisited;
+	unsigned int distance;
+	int previousNode[2];
+};
+
+int main()
+{
+	// geting image size from header
+	int width, height;
+	std::string line;
+	getline(std::cin, line);
+	std::cin >> width >> height;
+	getline(std::cin, line);
+	getline(std::cin, line);
+
+	// allocating image buffer and populating it
+	char imageBuffer[height][width][3];
+	for (int i = 0; i < height; i++)
+		for (int j = 0; j < width; j++)
+			for (int k = 0; k < 3; k++)
+			{
+				// std::cin.read(&imageBuffer[i][j][k], sizeof(char));
+				scanf("%c", &imageBuffer[i][j][k]);
+			}
+
+	// generating grid
+}
